d7917a512e6ebdfee7e3d91d04eae8ca9f64aaf2
petersem <matthew.petersem.68@gmail.com>
2026-03-08 17:10:26 +1000

    added optional function parameters

--- 2_functions.mjs
+++ 2_functions.mjs
@@ -17,3 +17,12 @@ const result = (num1, num2) => {
     return num1 + num2;
     };
 console.log(result(5,2));  // returns 7
+
+// Optional function values
+function hello(firstName, lastName = 'you lamer!') {
+    return "Hello " + firstName + " " + lastName;
+}
+
+console.log(hello("Matt", "P"));  // prints Hello Matt P
+
+console.log(hello("Matt"));  // prints Hello you lamer
