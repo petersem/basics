0382cc65af67d837374fd003a31ad7934c0f83c8
petersem <matthew.petersem.68@gmail.com>
2026-03-08 17:28:13 +1000

    promises added

---
+++ 9_promises.mjs
@@ -0,0 +1,32 @@
+// promises - are used to wait for data, then act on it. Otherwise JS will just continue running your code. 
+// So could be issues if you need something from a long operation
+
+
+// without prompises and async / await 
+function doALongTask() {
+    // wait 2 seconds to simulate a slow operation
+    setTimeout(() => {
+            console.log("First Long operation complete");
+        }, 1000);
+
+    console.log("Something done after first long operation");
+};
+
+doALongTask();  // JS doesn't block execution, and prints 'something done after first long operation', then 'First long operation complete'
+
+
+
+// async function with long op wrapped in a promise
+async function doSomeLongTask() {
+    await new Promise((resolve) => {  // waits for resolve before returning to other code
+        // wait 2 seconds to simulate a slow operation
+        setTimeout(() => {
+            console.log("Second Long operation complete");
+            resolve(); // calls resolve
+        }, 2000);
+    });
+
+    console.log("Something done after second long operation");
+}
+
+doSomeLongTask();  // waits for 'Second Long operation complete' to print, then displays 'Something done after second long operation'
