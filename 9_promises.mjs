f69493373eedcd0a7f62cab2d3a0d1046add7d51
petersem <matthew.petersem.68@gmail.com>
2026-03-15 14:31:26 +1000

    fix promise code

--- 9_promises.mjs
+++ 9_promises.mjs
@@ -3,57 +3,59 @@
 
 
 // without prompises and async / await 
-// function doALongTask() {
-//     // wait 1 seconds to simulate a slow operation
-//     setTimeout(() => {
-//             console.log("1");
-//         }, 1000);
+function doALongTask() {
+    // wait 1 seconds to simulate a slow operation
+    setTimeout(() => {
+            console.log("1");
+        }, 1000);
 
-//     console.log("2");
-// };
+    console.log("2");
+};
 
-// doALongTask();  
-// console.log('3');
+doALongTask();  
+console.log('3');
 
 
 // async function with long op wrapped in a promise
-// async function doSomeLongTask() {
-//     await new Promise((resolve) => {  // waits for resolve before returning to other code
-//         // wait 2 seconds to simulate a slow operation
-//         setTimeout(() => {
-//             console.log("1");
-//             resolve(); // calls resolve
-//         }, 2000);
-//     });
+async function doSomeLongTask() {
+    await new Promise((resolve) => {  // waits for resolve before returning to other code
+        // wait 2 seconds to simulate a slow operation
+        setTimeout(() => {
+            console.log("1");
+            resolve(); // calls resolve
+        }, 2000);
+    });
 
-//     console.log("2");
-// }
+    console.log("2");
+}
 
-// doSomeLongTask();  // waits for the long task function, then displays 1 and 2
-// console.log('3'); // This is outside the function, so js runs this while the long operation is waiting
+doSomeLongTask();  // waits for the long task function, then displays 1 and 2
+console.log('3'); // This is outside the function, so js runs this while the long operation is waiting
 
-// // now use the .then feature
-// async function doThenLongTask() {
-//     let poop = 5;
-//     await new Promise((resolve) => {  // waits for resolve before returning to other code
-//         // wait 2 seconds to simulate a slow operation
-//         setTimeout(() => {
-//             poop++
-//             resolve();
-//         }, 2000);
-//     });
-//     return poop;
-// };
+// now use the .then feature
+async function doThenLongTask() {
+    let poop = 5;
+    await new Promise((resolve) => {  // waits for resolve before returning to other code
+        // wait 2 seconds to simulate a slow operation
+        setTimeout(() => {
+            poop++
+            resolve();
+        }, 2000);
+    });
+    return poop;
+};
 
 
-// doThenLongTask()
-//     .then((data) => {
-//         console.log(data);
-//     })
-//     .catch(function (error) {
-//         console.log(error)
-//     });
+doThenLongTask()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
 
+
+// do an async call to a jokes API, then wait to print the response.
 fetch('https://icanhazdadjoke.com/', {
   headers: {
     "User-Agent": "Matt",
