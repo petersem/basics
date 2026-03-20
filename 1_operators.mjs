9e58cda0faa5491cfdb1d5b09c1d118e71f529ec
petersem <matthew.petersem.68@gmail.com>
2026-03-20 12:54:24 +1000

    added operators

--- 1_operators.mjs
+++ 1_operators.mjs
@@ -1,5 +1,11 @@
-// Assignment of value
-let a = 1; 
+// Assignment operators
+let a = 1; // simple assign
+a += 2; // same as a = a + 2
+a -= 2; // same as a = a - 2
+a *= 2; // same as a = a * 2
+a /= 2; // same as a = a / 2
+a **= 3; // same as a = a * a * (exponent)
+a %= 2; // same as a = a % 2  (modulus)
 
 // Arithmatic operators
 a = a + 1; // add
@@ -12,4 +18,14 @@ let b = ++a // increment by 1 before assignment
 b = a++ // increment by 1 after assignment
 b = --a // decrement by 1 before assignment
 b = a-- // decrement by 1 after assignment
+
+// Comparrison operators
+console.log( true == 1 ); // equal to value - true as boolean values like true/false are also represented as 1 and 0 respectively
+console.log( true === 1 ); // equal to value and type - false as values are the same but boolean and number are different types2
+console.log( 1 != 2 ); // not equal to value - false
+console.log( true !== 1 ); // not equal to value and type - true as not equal to value and type
+console.log( 2 > 2 ); // greater than - false as 2 is not greater than 2 
+console.log( 2 >= 2 ); // greater than or equal to - true as 2 is equal 2 
+console.log( 1 < 2 ); // less than - true as 1 is less than 2
+console.log( 2 <= 2 ); // less than or equal to - true as 2 is equal to 2 
 
