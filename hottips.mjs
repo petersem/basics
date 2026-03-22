4a187dd35849fd9247b80656c9ce79371a99302d
petersem <matthew.petersem.68@gmail.com>
2026-03-22 12:48:44 +1000

    cleaned up comments

--- hottips.mjs
+++ hottips.mjs
@@ -1,8 +1,6 @@
-/**
- * get the weekday for a given date
- * @param {date} forDate - for a given date
- * @returns the Day of the week
- */
+console.log(`
+-------------- Get weekday for a given date`)
+
 function getWeekDay(forDate) {
     const days = [
         "Sunday",
@@ -20,8 +18,8 @@ function getWeekDay(forDate) {
 console.log(getWeekDay("2026-02-20"));  // get the day for a given date
 console.log(getWeekDay(new Date()));  // get todays day
 
-// ---------------------------------------------------------------------------
-
+console.log(`
+-------------- Returns a number within a range`)
 /**
  * Retuns a number within a defined min/max range
  * @param {number} num - input number
@@ -38,8 +36,8 @@ console.log(clamp(-10, 0, 5)); // outside range. closest value is 0
 console.log(clamp(3, 0, 5)); // in range. Just return 3
 console.log();
 
-// ---------------------------------------------------------------------------
-
+console.log(`
+-------------- Get ordinal descriptor for a given day of the month`)
 /**
  * Returns the ordinal descriptor for a given day number.
  * Examples: 1 -> "1st", 2 -> "2nd", 3 -> "3rd", 4 -> "4th"
@@ -78,8 +76,9 @@ const day = d.getDate();  // gets day of the month
 console.log(getDaySuffix(day));   // For today
 console.log();
 
-// ---------------------------------------------------------------------------
 
+console.log(`
+-------------- Get a random UUID`)
 import crypto from 'crypto';
 
 /**
@@ -94,15 +93,11 @@ function generateUUID() {
 console.log("Generated UUID:", generateUUID());
 console.log();
 
-// ---------------------------------------------------------------------------
-
+console.log(`
+-------------- Reading environment variables`)
 // Read environment variables from .env file, but only if not in production mode.
 // must use `node --env-file=.env xxx.mjs`
 
-// Set environment values in your .env file, then read them into your code at run time
-// if production, these values will be set at the OS. 
-// try it! npm run dev    and    npm run prod
-// prod will have undefined output as not set on the OS for GREETING and SECRET_PSW
 console.log(`Reading from Environment variables: 
     ${process.env.GREETING}
     ${process.env.SECRET_PSW}
@@ -110,8 +105,7 @@ console.log(`Reading from Environment variables:
 `);
 console.log();
 
-// ---------------------------------------------------------------------------
-
+// --------------------------------------------------------
 // Reading and writing to files
 // 
 import fs from 'fs';
@@ -119,19 +113,21 @@ import fs from 'fs';
 // Read a file (asynchronous)
 fs.readFile('simple.txt', 'utf8', (err, data) => {
     if (err) return console.error('Error reading file:', err);
-    console.log();
+    console.log(`
+-------------- Reading files`)
     console.log('File contents read:', data);
 });
 
 // Write to a file
 fs.writeFile('new.txt', 'Hello Node.js!', (err) => {
     if (err) return console.error('Error writing file:', err);
-    console.log();
+    console.log(`
+-------------- Writing files`)
     console.log('File written successfully.');
 });
 
-// ---------------------------------------------------------------------------
-
+console.log(`
+-------------- Getting age from birthday`)
 // Get someones age from birthdate
 
 function getAge(birthDateString) {
@@ -161,20 +157,23 @@ function getAge(birthDateString) {
 console.log(getAge("1998-08-13")); // get age
 console.log();
 
-// ---------------------------------------------------------------------------
 
-// Events emitter
-//
+// -------------------------------------------------------------------
 import EventEmitter from 'events';
 const emitter = new EventEmitter();
 
-emitter.on('greet', (name) => console.log(`Hello, ${name}!`));
+emitter.on('greet', (name) => {
+    console.log(`
+-------------- Events and event emitters`)// Events emitter
+    console.log(`Hello, ${name}!`)
+});
 
 // put the event trigger in a timer to make it run after other examples do
 setTimeout(() => emitter.emit('greet', 'Fred'), 14000);
 
-// ---------------------------------------------------------------------------
 
+console.log(`
+-------------- Console formatting`)
 // Console Formatting - https://nodejs.org/api/util.html#customizing-utilinspect-colors
 
 import { styleText } from 'node:util';
@@ -189,8 +188,8 @@ console.log(logWarning,'uh oh');
 console.log(logInfo,'Well then..');
 console.log();
 
-// ---------------------------------------------------------------------------
 
+// -------------------------------------
 // Creating an object, serialising it, saving it to a file, then reading it back in
 //
 
@@ -209,6 +208,8 @@ let mySettings2 = {};
 fs.readFile('settings.json', 'utf8', (err, data) => {
     if (err) return console.error('Error reading file:', err);
     mySettings2 = JSON.parse(data);
+console.log(`
+-------------- Reading file contents into an object`);
     console.log('Setting file contents read into object');
     console.log('Test object read: ' + mySettings2.age);
     console.log();
@@ -217,19 +218,22 @@ fs.readFile('settings.json', 'utf8', (err, data) => {
 // serialise setting object and write to file
 fs.writeFile('settings.json',JSON.stringify(mySettings), (err) => {
     if (err) return console.error('Error writing file:', err);
-    console.log();
+    console.log(`
+-------------- Write object to a file`);
     console.log('Settings written successfully to settings.json.');
 });
 
 
+console.log(`
+-------------- Print object as a table`);
 // console.table() - prints objects and arrays in a table format
 console.log('Output for: console.table(object)')
 console.table(mySettings);
 
 
-
+console.log(`
+-------------- Itterate through properties of an object`);
 // itterate through all properties of an object
-
 // create a fake body object
 const fakeBody = { 
     jigery: "pokery",
@@ -243,8 +247,9 @@ for (const [key, value] of Object.entries(fakeBody)) {
     // safe = false, unsafe = true
     console.log(fakeBody[key], /[$&+,:;=?@#|'<>.^*()%!-]/g.test(fakeBody[key]));
 }
-console.log('----------------------');
 
+console.log(`
+-------------- Output messages as a group`);
 // group content auto-indented under group heading
 console.group("User Info");
 console.log("Name: Matt");
@@ -257,7 +262,8 @@ setTimeout(() => {
     console.timeEnd("5sec");
 }, 5000);
 
-
+console.log(`
+-------------- Hash strings to compare for changes`);
 // hash an object to compare if it has changed
 function hashRequest(thing) {
   const content = JSON.stringify(thing) || '';
@@ -269,6 +275,32 @@ let fir = hashRequest(poop);
 poop = "dog";
 let sec = hashRequest(poop);
 let thi = hashRequest("dog");
-console.log("------" + fir);
-console.log("------" + sec); // has changes
-console.log("------" + thi); // same as changed object
+console.log(fir);
+console.log(sec); // has changes
+console.log(thi); // same as changed object
+
+console.log(`
+-------------- Doing a bubble sort`);
+// Doing a bubble sort - compare-swap-repeat
+const sortArr = [12,1,3,3,6,13,8,5,3,5];
+
+const bubbleSort = (arr) => {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+};
+
+console.log(sortArr); // pre-sort
+console.log(bubbleSort(sortArr)); // post-sort
